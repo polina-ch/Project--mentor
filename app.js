da43d3e98debe7af6e18b4e8566d97f057671ad0
Evgeniy <JenyaUtkin90@yandex.ru>
2022-01-12 17:59:34 +0300

    release

---
+++ app.js
@@ -0,0 +1,31 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+const PORT = 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(PORT, () => {
+  console.log('Server start ====>>>', PORT)
+})
+
